0206922ae8f4be94e7e2ae423ed7544cedc7d359
Gabe Weintraub <gabe.weintraub@gmail.com>
2015-11-16 10:27:17 -0500

    functional twitbot

---
+++ config.js
@@ -0,0 +1,12 @@
+// Change this file to config.js
+// Add your keys
+// Add file .gitignore: config.js
+// Load with 
+// var config = require('./config.js');
+
+module.exports = {
+  consumer_key:         'zNk7laSfvFMaXjXY2LoPgy1hm', 
+  consumer_secret:      'IT22LeyUM2KOZa8SXDaxmFnkjSLoHy9FYqfGOR7iW80TVPICLc',
+  access_token:         '4202532387-0gKdUAGI2tJcVcKzbHTzENo5ah6mDTOkxK7lp09',
+  access_token_secret:  'IjWnJdPJbXZJOfad7DqbMASGOX91RQf8CJQtjGrXt1STK'
+}
